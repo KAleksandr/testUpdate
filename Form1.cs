6d1874fbffb6ab1d4e33308af5ff6a5a71ffcfb2
Oleksandr Kuzmenko <okuzmenkoa@gmail.com>
2019-06-26 17:45:34 +0300

    test1

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Reflection;
@@ -10,7 +12,9 @@ namespace progressbar
     public partial class Form1 : Form
     {
         private string path2 = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"version.xml");
-        private int thisVersion = Convert.ToInt32(Assembly.GetExecutingAssembly().GetName().Version.ToString().Replace(".",""));
+        private string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"update.exe");
+        private int thisVersion = Convert.ToInt32(Assembly.GetExecutingAssembly().GetName().Version.ToString().Replace(".", ""));
+        private string thisVersionS = (Assembly.GetExecutingAssembly().GetName().Version.ToString());
         public Form1()
         {
             InitializeComponent();
@@ -18,29 +22,90 @@ namespace progressbar
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Download();
+            checkUpdates();
+        }
+        private void download_Completed()
+        {
+            try
+            {
+                Process.Start("updater.exe", "update.exe progressbar.exe");
+                Process.GetCurrentProcess().Kill();
+                
+            }
+            catch (Exception) { }
+        }
+
+        public void Download()
+        {
+            string url = "https://raw.githubusercontent.com/KAleksandr/testUpdate/master/version.xml";
+            string url2 = "https://github.com/KAleksandr/testUpdate/blob/master/progressbar.exe?raw=true";
+
+
+            int ver;
+            string version;
+            var doc = new XmlDocument();
             using (WebClient wc = new WebClient())
             {
-                string url= "https://raw.githubusercontent.com/KAleksandr/testUpdate/master/version.xml";
+
                 wc.DownloadProgressChanged += (s, te) => { progressBar1.Value = te.ProgressPercentage; };
                 //wc.DownloadFileAsync(new Uri( "https://download.virtualbox.org/virtualbox/6.0.8/VirtualBox-6.0.8-130520-Win.exe"), $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\VirtualBox 6.0.8.exe");
                 //wc.DownloadFileAsync(new Uri(url),path2);
-                wc.DownloadFile(new Uri(url),path2);
-                var doc = new XmlDocument();
-                doc.Load(path2);
-                string version = doc.GetElementsByTagName("myprogram")[0].InnerText;
-                int ver = Convert.ToInt32(version.Replace(".", ""));
-                if (thisVersion < ver)
+                wc.DownloadFile(new Uri(url), path2);
+                bool exists = System.IO.Directory.Exists(path2);
+                if (!exists)
                 {
-                    MessageBox.Show("new version " + version);
-                    MessageBox.Show(this, "Виявлено нову версію (" + doc.GetElementsByTagName("myprogram")[0].InnerText + ")" + Environment.NewLine +
-                                          "Додаток буде автоматично оновлено і перезапуститься.", Application.ProductName + " v" + Application.ProductVersion, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    doc.Load(path2);
+                    version = doc.GetElementsByTagName("myprogram")[0].InnerText;
+                    ver = Convert.ToInt32(version.Replace(".", ""));
                 }
+                else
+                {
+                    ver = thisVersion;
+                    version = thisVersionS;
+                }
+
+
+
+            }
+            if (thisVersion < ver)
+            {
+                if (File.Exists("launcher.update")) { File.Delete("launcher.update"); }
+                MessageBox.Show("new version " + version);
+                MessageBox.Show(this, "Виявлено нову версію (" + doc.GetElementsByTagName("myprogram")[0].InnerText + ")" + Environment.NewLine +
+                                      "Додаток буде автоматично оновлено і перезапуститься.", Application.ProductName + " v" + Application.ProductVersion, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                using (WebClient wc = new WebClient())
+                {
+                    // wc.DownloadProgressChanged += (s, te) => { progressBar1.Value = te.ProgressPercentage; };
+                    wc.DownloadFile(new Uri(url2), "launcher.update");
+                }
+
+
                 
+
             }
         }
-
-        
-
+        public void checkUpdates()
+        {
+            try
+            {
+                if (File.Exists("launcher.update") && new Version(FileVersionInfo.GetVersionInfo("launcher.update").FileVersion) > new Version(Application.ProductVersion))
+                {
+                    Process.Start("updater.exe", "progressbar.exe  launcher.update");
+                    Process.GetCurrentProcess().CloseMainWindow();
+                }
+                else
+                {
+                    if (File.Exists("launcher.update")) { File.Delete("launcher.update"); }
+                    Download();
+                }
+            }
+            catch (Exception)
+            {
+                if (File.Exists("launcher.update")) { File.Delete("launcher.update"); }
+                Download();
+            }
+        }
 
     }
 
