83ff8c40e87b66387e99b3ecaff0fb8c7b0d4f6c
Oleksandr Kuzmenko <okuzmenkoa@gmail.com>
2019-06-26 15:56:34 +0300

    first commit

---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace progressbar
+{
+    public partial class Form1 : Form
+    {
+        private string path2 = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"version.xml");
+        private int thisVersion = Convert.ToInt32(Assembly.GetExecutingAssembly().GetName().Version.ToString().Replace(".",""));
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                string url= "https://raw.githubusercontent.com/KAleksandr/PayConverter2/master/version.xml";
+                wc.DownloadProgressChanged += (s, te) => { progressBar1.Value = te.ProgressPercentage; };
+                //wc.DownloadFileAsync(new Uri( "https://download.virtualbox.org/virtualbox/6.0.8/VirtualBox-6.0.8-130520-Win.exe"), $@"{Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}\\VirtualBox 6.0.8.exe");
+                //wc.DownloadFileAsync(new Uri(url),path2);
+                //wc.DownloadFile(new Uri(url),path2);
+                var doc = new XmlDocument();
+                doc.Load(path2);
+                string version = doc.GetElementsByTagName("myprogram")[0].InnerText;
+                int ver = Convert.ToInt32(version.Replace(".", ""));
+                if (thisVersion < ver)
+                {
+                    MessageBox.Show("new version " + version);
+                    MessageBox.Show(this, "Виявлено нову версію (" + doc.GetElementsByTagName("myprogram")[0].InnerText + ")" + Environment.NewLine +
+                                          "Додаток буде автоматично оновлено і перезапуститься.", Application.ProductName + " v" + Application.ProductVersion, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                
+            }
+        }
+
+        
+
+
+    }
+
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("progressbar")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("progressbar")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("784b293b-3ab3-4716-a108-71ef5abc76f3")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
